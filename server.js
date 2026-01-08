d9b899203eae30608da92e2b32f602848f057104
Karm Pandya <KarmPandya007@users.noreply.github.com>
2026-01-08 16:24:08 +0530

    Fixed all the issues

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 await connectDB();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('API is running...');
